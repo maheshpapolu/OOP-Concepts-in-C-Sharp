6564b6a54f0cce805c0dc059f3507549738e95c2
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:36:16 +0530

    added abstraction files

---
+++ Abstraction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    // Abstract class
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Dog: Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The Dog says: Bow Bow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();  // Create a Pig object
+            myDog.animalSound();
+            myDog.sleep();
+        }
+    }
+}
