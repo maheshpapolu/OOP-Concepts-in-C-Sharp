c2ceb3046810867e55b8bdb522fbcda340643f83
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:20:20 +0530

    added  Class members in OOP files

---
+++ Class members in OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_members_in_OOP
+{
+     class Car 
+    {
+        string Colour;  //field
+        int maxSpeed;   //field
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.Colour = "White";
+            myObj.maxSpeed = 200;
+
+            Console.WriteLine(myObj.Colour);
+            Console.WriteLine(myObj.maxSpeed);
+
+        }
+    }
+}
