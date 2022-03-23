6093511ac0f2237c7565b918833d551c30ce9094
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:24:24 +0530

    added Object method in OOP files

---
+++ Object method in OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_method_in_OOP
+{
+     class Car
+    { 
+        string color;                 // field
+        int maxSpeed;                 // field
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.fullThrottle();  // Call the method
+        }
+    }
+}
