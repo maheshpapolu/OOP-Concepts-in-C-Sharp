b60e90750987c6d756df3a2bae12b373a54785d3
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:16:30 +0530

    added  Classes and Objects in OOP files

---
+++ Classes and Objects in OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects_in_OOP
+{
+     class Car  // create class named as Car
+    { 
+        string colour = "White";  // varible Colour
+    
+        static void Main(string[] args)
+        {
+                Car myObj1 = new Car();   // Create an Object called myObj
+                Car myObj2 = new Car();
+                Console.WriteLine(myObj1.colour);  // used object to print the value of colour
+                Console.WriteLine(myObj2.colour);
+            }
+        }
+    }
+    
+
