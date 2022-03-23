734e9818bfea25d6dede835fa4d85c8bdafe35a5
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:30:21 +0530

    added  Properties and Encapsulation in OOP files

---
+++ Properties and Encapsulation in OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_and_Encapsulation_in_OOP
+{
+    class Person
+    {
+        private string name;        // Create a field
+        public string Name          // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    class program
+    { 
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Jhon";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
