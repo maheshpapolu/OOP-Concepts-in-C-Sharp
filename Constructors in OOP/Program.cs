4a03e61ebcd20d98d23c4d07e64eae1e1a2aea15
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-24 02:27:50 +0530

    added Constructors in OOP files

---
+++ Constructors in OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_in_OOP
+{
+    class Car                                   // Create a car class
+    {
+        public string model;                    // create a field
+
+        public Car()                            // create a class constructor for the car class
+        {
+            model = "Mustang";                  // set the initial value for model
+        }
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();               // call the constructor
+
+            Console.WriteLine(Ford.model);     // Print the value of model
+        }
+    }
+}
